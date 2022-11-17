89b55223e69ef6e38b59d17c1cd3333f364f3159
AdonyasG <adonyas2000@gmail.com>
2022-11-17 15:14:50 -0500

    helper func

---
+++ helper.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * max- helper func
+ * @a: variable to compute
+ * @b: variable to compute
+ * Return: computed val
+ */
+size_t max(int a, int b)
+{
+	return ((a > b) ? a : b);
+}
