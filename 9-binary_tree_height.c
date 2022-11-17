134846e2b295b625d4e296dd909d43b20c0dd5f4
AdonyasG <adonyas2000@gmail.com>
2022-11-17 15:11:16 -0500

    computes the depth

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height- function to calculate a binary tree depth
+ * @tree: pointer to the root node
+ * Return: NULL if an a error occurs
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	return (1 + max(binary_tree_height(tree->left),
+			binary_tree_height(tree->right)));
+}
