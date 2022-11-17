4cb920c49557b568684af7905462153848fdc7c8
AdonyasG <adonyas2000@gmail.com>
2022-11-17 15:33:01 -0500

    return val fix

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,10 +7,10 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t num = 1;
-	
+
 	if (tree == NULL)
 		return (0);
-	else
-		return (binary_tree_size(tree->left) + num + binary_tree_size(tree->right));
-	
+	num += binary_tree_size(tree->left);
+	num += binary_tree_size(tree->right);
+	return (num);
 }
