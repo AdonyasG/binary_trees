5b11695a63193e76db04d0cedc7681f96c6d6729
AdonyasG <adonyas2000@gmail.com>
2022-11-17 15:24:37 -0500

    removed the parentheses

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,7 +11,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	count += ((tree->left || tree->right) ? 1 : 0);
+	count += (tree->left || tree->right) ? 1 : 0;
 	count += binary_tree_nodes(tree->left);
 	count += binary_tree_nodes(tree->left);
 	return (count);
